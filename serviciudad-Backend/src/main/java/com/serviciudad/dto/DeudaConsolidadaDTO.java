7390da9212330c2bf02a35d209a9025be39f7d13
Jean Paul Delgado <jean.delgado@uao.edu.co>
2025-10-23 17:19:09 -0500

    fix: remove boilerplate code provided by Lombok

--- serviciudad-Backend/src/main/java/com/serviciudad/dto/DeudaConsolidadaDTO.java
+++ serviciudad-Backend/src/main/java/com/serviciudad/dto/DeudaConsolidadaDTO.java
@@ -1,9 +1,13 @@
 package com.serviciudad.dto;
 
+import lombok.Data;
+
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
+@Data
 public class DeudaConsolidadaDTO {
+
     private String clienteId;
     private String nombreCliente;
     private OffsetDateTime fechaConsulta;
@@ -11,16 +15,4 @@ public class DeudaConsolidadaDTO {
     private DetalleServicioDTO acueducto;
     private BigDecimal totalAPagar;
 
-    public String getClienteId() { return clienteId; }
-    public void setClienteId(String clienteId) { this.clienteId = clienteId; }
-    public String getNombreCliente() { return nombreCliente; }
-    public void setNombreCliente(String nombreCliente) { this.nombreCliente = nombreCliente; }
-    public OffsetDateTime getFechaConsulta() { return fechaConsulta; }
-    public void setFechaConsulta(OffsetDateTime fechaConsulta) { this.fechaConsulta = fechaConsulta; }
-    public DetalleServicioDTO getEnergia() { return energia; }
-    public void setEnergia(DetalleServicioDTO energia) { this.energia = energia; }
-    public DetalleServicioDTO getAcueducto() { return acueducto; }
-    public void setAcueducto(DetalleServicioDTO acueducto) { this.acueducto = acueducto; }
-    public BigDecimal getTotalAPagar() { return totalAPagar; }
-    public void setTotalAPagar(BigDecimal totalAPagar) { this.totalAPagar = totalAPagar; }
 }
