1dc722d56cb79254d4f93c3890f327a7bdab8a08
Jean Paul Delgado <jean.delgado@uao.edu.co>
2025-11-05 18:30:36 -0500

    fix: add lines  to validate ci pipeline

--- serviCiudad-Frontend/src/app/app.spec.ts
+++ serviCiudad-Frontend/src/app/app.spec.ts
@@ -1,12 +1,17 @@
 import { provideZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
 import { App } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-      providers: [provideZonelessChangeDetection()]
+      providers: [
+        provideZonelessChangeDetection(),
+        provideRouter(routes)
+      ]
     }).compileComponents();
   });
 
